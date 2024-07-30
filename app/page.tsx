c6ffcd43b42fb1b142b62b655b3b96b7f4e10222
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-07-30 15:23:38 +0530

    routing completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
-export default function Home() {
+function HomePage() {
   return (
-   <>
-      hello
-   </>
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>
+        Click me
+      </Button>
+    </div>
   );
 }
+export default HomePage;
